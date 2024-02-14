982d7b0bae49aa6c9ddd51df874d0ec3a591336c
HMTBOSS <160050284+HMTBOSS@users.noreply.github.com>
2024-02-14 19:00:53 +0200

    Add files via upload

---
+++ ft_print_numbers.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+
+void ft_print_numbers(void)
+{
+    short   n;
+    
+    n = 47;
+    while (++n < 58)
+        write(1, &n, 1);
+}
+
+void ft_print_numbers2(void)
+{
+    short n;
+    
+    n = '/';
+    while (++n < ':')
+        write(1, &n, 1);
+    
+}
+
+int main(void)
+{
+    ft_print_numbers();
+    write(1,  "\n", 1);
+    
+}
